f24edc62e918ce8246b239d531c7e7f75817dc71
Dale1003 <deiru03@gmail.com>
2025-01-11 19:39:03 +0800

    fix show and unshow password icon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,16 +25,19 @@
         <div class="mb-4 relative">
             <x-input-label for="password" :value="__('Password')" class="text-sm font-medium text-gray-700" />
             <div class="relative">
-                <x-text-input id="password" class="mt-1 block w-full pr-4"
+                <x-text-input id="password" class="mt-1 block w-full pr-10"
                                 type="password"
                                 name="password"
                                 required autocomplete="current-password" />
-                {{-- <button type="button" class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5" onclick="togglePasswordVisibility('password')">
-                    <svg class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <button type="button" class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5" onclick="togglePasswordVisibility('password')">
+                    <svg class="h-5 w-5 text-gray-500 show-password" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                     </svg>
-                </button> --}}
+                    <svg class="h-5 w-5 text-gray-500 hide-password hidden" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
+                    </svg>
+                </button>
             </div>
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -86,11 +89,18 @@
 
     <script>
         function togglePasswordVisibility(inputId) {
-            var input = document.getElementById(inputId);
+            const input = document.getElementById(inputId);
+            const showIcon = input.nextElementSibling.querySelector('.show-password');
+            const hideIcon = input.nextElementSibling.querySelector('.hide-password');
+            
             if (input.type === "password") {
                 input.type = "text";
+                showIcon.classList.add('hidden');
+                hideIcon.classList.remove('hidden');
             } else {
                 input.type = "password";
+                showIcon.classList.remove('hidden');
+                hideIcon.classList.add('hidden');
             }
         }
     </script>
